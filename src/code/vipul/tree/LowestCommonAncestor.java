59278c4660448b48a19f5c74bf5392c7b1b2bf45
Gaur, Vipul <vgaur@cvent.com>
2018-12-11 13:52:04 +0530

    Simplify lca code a bit

--- src/code/vipul/tree/LowestCommonAncestor.java
+++ src/code/vipul/tree/LowestCommonAncestor.java
@@ -20,42 +20,35 @@ public class LowestCommonAncestor {
     public static Result findLca(TreeNode node, int v1, int v2) {
         if (node == null) return Result.DEFAULT;
 
-        boolean isCurrentV1 = node.getValue() == v1;
-        boolean isCurrentV2 = node.getValue() == v2;
+        if (node.getValue() == v1 && node.getValue() == v2) Result.lca(node.getValue());
 
-        if (isCurrentV1 && isCurrentV2) return Result.lca(node.getValue());
+        boolean isNodeValue = node.getValue() == v1 || node.getValue() == v2;
 
         Result right = findLca(node.right, v1, v2);
         if (right.lcaFound()) return right;
         Result left = findLca(node.left, v1, v2);
         if (left.lcaFound()) return left;
 
-        if ((right.isV1() && left.isV2()) || (right.isV2() && left.isV1())) {
+        if (right.valueFound() && left.valueFound()) {
             return Result.lca(node.getValue());
         }
 
-        boolean areChildrenV1 = right.isV1() || left.isV1();
-        boolean areChildrenV2 = right.isV2() || left.isV2();
+        boolean areChildrenValue = right.valueFound() || left.valueFound();
 
-        if ((areChildrenV1 && isCurrentV2) || (areChildrenV2 && isCurrentV1)) {
+        if (areChildrenValue && isNodeValue) {
             return Result.lca(node.getValue());
         }
 
-        return Result.result(areChildrenV1 || isCurrentV1, areChildrenV2 || isCurrentV2);
+        return Result.result(areChildrenValue || isNodeValue);
     }
 
     public static final class Result {
 
-        private final boolean v1;
-        private final boolean v2;
+        private final boolean vFound;
         private final Integer value;
 
-        public boolean isV1() {
-            return v1;
-        }
-
-        public boolean isV2() {
-            return v2;
+        public boolean valueFound() {
+            return vFound;
         }
 
         public Integer getValue() {
@@ -66,20 +59,19 @@ public class LowestCommonAncestor {
             return value != null;
         }
 
-        public static final Result DEFAULT = new Result(false, false, null);
+        public static final Result DEFAULT = new Result(false, null);
 
-        private Result(boolean v1, boolean v2, Integer value) {
-            this.v1 = v1;
-            this.v2 = v2;
+        private Result(boolean v, Integer value) {
+            this.vFound = v;
             this.value = value;
         }
 
-        public static Result result(boolean v1, boolean v2) {
-            return new Result(v1, v2, null);
+        public static Result result(boolean v) {
+            return new Result(v, null);
         }
 
         public static Result lca(Integer lca) {
-            return new Result(false, false, lca);
+            return new Result(false, lca);
         }
     }
 }
