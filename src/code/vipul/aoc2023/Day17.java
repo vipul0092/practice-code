d3af4cd86f37edb6aaee92037d1f5fa1bebb2d01
Vipul Gaur <sunny3091@gmail.com>
2023-12-18 12:06:50 +0530

    Improved and cleaned up the solution for Day 17 AoC 2023

--- src/code/vipul/aoc2023/Day17.java
+++ src/code/vipul/aoc2023/Day17.java
@@ -36,156 +36,40 @@ public class Day17 {
     record Point(int x, int y){}
     record Pwd(Point p, int dir){}
     private static final int INF = Integer.MAX_VALUE;
-
     private static int ROWS = -1, COLS = -1;
 
-    static int[] diff = new int[]{1, 2, 3};
-    static int[] diff2 = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    // dir == 1 means hor, == 2 ver
+    private static final int VERTICAL = 1, HORIZONTAL = 0;
+
     private static Map<Pwd, Map<Pwd, Integer>> adjacencyListWithWeights;
     private static Map<Pwd, Integer> distanceMap;
     public static void solve() {
         INPUT = DAY_17;
         List<String> lines = Arrays.stream(INPUT.split("\n")).toList();
-        ROWS = lines.size();
-        COLS = lines.get(0).length();
-        adjacencyListWithWeights = new LinkedHashMap<>();
-        distanceMap = new LinkedHashMap<>();
 
-        for (int i = 0; i < lines.size(); i++) {
-            for (int j = 0; j < lines.get(0).length(); j++) {
-                // vertical
-                Pwd hor = new Pwd(new Point(i, j), 2);
-                distanceMap.put(hor, INF);
-                adjacencyListWithWeights.put(hor, new LinkedHashMap<>());
-                int distancep = 0;
-                int distancen = 0;
-                for (int d : diff) {
-                    int jj = j + d;
-                    if (valid(i, jj, lines)) {
-                        distancep += valueAt(i, jj, lines);
-                        adjacencyListWithWeights.get(hor).put(new Pwd(new Point(i, jj), 1), distancep);
-                    }
-                    jj = j - d;
-                    if (valid(i, jj, lines)) {
-                        distancen += valueAt(i, jj, lines);
-                        adjacencyListWithWeights.get(hor).put(new Pwd(new Point(i, jj), 1), distancen);
-                    }
-                }
-
-                // horizontal
-                Pwd ver = new Pwd(new Point(i, j), 1);
-                distanceMap.put(ver, INF);
-                adjacencyListWithWeights.put(ver, new LinkedHashMap<>());
-                distancep = 0; distancen = 0;
-                for (int d : diff) {
-                    int ii = i + d;
-                    if (valid(ii, j, lines)) {
-                        distancep += valueAt(ii, j, lines);
-                        adjacencyListWithWeights.get(ver).put(new Pwd(new Point(ii, j), 2), distancep);
-                    }
-                    ii = i - d;
-                    if (valid(ii, j, lines)) {
-                        distancen += valueAt(ii, j, lines);
-                        adjacencyListWithWeights.get(ver).put(new Pwd(new Point(ii, j), 2), distancen);
-                    }
-                }
-            }
-        }
-
+        populateGraph(lines, 0, 3);
         Map<Pwd, Integer> distanceCopy = new HashMap<>(distanceMap);
-        Pwd start = new Pwd(new Point(0, 0), 1);
-        distanceMap.put(start, 0);
-        int min1 = dijkstra(start, new Point(ROWS-1, COLS-1));
-
+        int min1 = dijkstra(new Pwd(new Point(0, 0), HORIZONTAL), new Point(ROWS-1, COLS-1));
         distanceMap = new HashMap<>(distanceCopy);
-        start = new Pwd(new Point(0, 0), 2);
-        distanceMap.put(start, 0);
-        int min2 = dijkstra(start, new Point(ROWS-1, COLS-1));
+        int min2 = dijkstra(new Pwd(new Point(0, 0), VERTICAL), new Point(ROWS-1, COLS-1));
+        System.out.println("Part 1: " + Math.min(min1, min2)); // 638
 
-        System.out.println(Math.min(min1, min2));
-        System.out.println();
-    }
-
-    public static void solvePart2() {
-        INPUT = DAY_17;
-        List<String> lines = Arrays.stream(INPUT.split("\n")).toList();
-        ROWS = lines.size();
-        COLS = lines.get(0).length();
-        adjacencyListWithWeights = new LinkedHashMap<>();
-        distanceMap = new LinkedHashMap<>();
-
-        for (int i = 0; i < lines.size(); i++) {
-            for (int j = 0; j < lines.get(0).length(); j++) {
-                // vertical
-                Pwd hor = new Pwd(new Point(i, j), 2);
-                distanceMap.put(hor, INF);
-                adjacencyListWithWeights.put(hor, new LinkedHashMap<>());
-                int distancep = 0;
-                int distancen = 0;
-                for (int d : diff2) {
-                    int jj = j + d;
-                    if (valid(i, jj, lines)) {
-                        distancep += valueAt(i, jj, lines);
-                        if (d > 3) {
-                            adjacencyListWithWeights.get(hor).put(new Pwd(new Point(i, jj), 1), distancep);
-                        }
-                    }
-                    jj = j - d;
-                    if (valid(i, jj, lines)) {
-                        distancen += valueAt(i, jj, lines);
-                        if (d > 3) {
-                            adjacencyListWithWeights.get(hor).put(new Pwd(new Point(i, jj), 1), distancen);
-                        }
-                    }
-                }
-
-                // horizontal
-                Pwd ver = new Pwd(new Point(i, j), 1);
-                distanceMap.put(ver, INF);
-                adjacencyListWithWeights.put(ver, new LinkedHashMap<>());
-                distancep = 0; distancen = 0;
-                for (int d : diff2) {
-                    int ii = i + d;
-                    if (valid(ii, j, lines)) {
-                        distancep += valueAt(ii, j, lines);
-                        if (d > 3) {
-                            adjacencyListWithWeights.get(ver).put(new Pwd(new Point(ii, j), 2), distancep);
-                        }
-                    }
-                    ii = i - d;
-                    if (valid(ii, j, lines)) {
-                        distancen += valueAt(ii, j, lines);
-                        if (d > 3) {
-                            adjacencyListWithWeights.get(ver).put(new Pwd(new Point(ii, j), 2), distancen);
-                        }
-                    }
-                }
-            }
-        }
-
-        Map<Pwd, Integer> distanceCopy = new HashMap<>(distanceMap);
-        Pwd start = new Pwd(new Point(0, 0), 1);
-        distanceMap.put(start, 0);
-        int min1 = dijkstra(start, new Point(ROWS-1, COLS-1));
-
+        // -------- Part 2 Begins --------
+        populateGraph(lines, 3, 10);
+        distanceCopy = new HashMap<>(distanceMap);
+        min1 = dijkstra(new Pwd(new Point(0, 0), HORIZONTAL), new Point(ROWS-1, COLS-1));
         distanceMap = new HashMap<>(distanceCopy);
-        start = new Pwd(new Point(0, 0), 2);
-        distanceMap.put(start, 0);
-        int min2 = dijkstra(start, new Point(ROWS-1, COLS-1));
-
-        System.out.println(Math.min(min1, min2));
-        System.out.println();
+        min2 = dijkstra(new Pwd(new Point(0, 0), VERTICAL), new Point(ROWS-1, COLS-1));
+        System.out.println("Part 2: " + Math.min(min1, min2)); // 748
     }
 
     private static int dijkstra(Pwd start, Point end) {
         int length = -1;
-
+        distanceMap.put(start, 0);
         Pwd current = start;
 
         PriorityQueue<Pair<Integer, Pwd>> minheap = new PriorityQueue<>(Comparator.comparingInt(Pair::left));
         minheap.add(Pair.of(distanceMap.get(current), start));
-        boolean[][][] visited = new boolean[ROWS+1][COLS+1][3];
+        boolean[][][] visited = new boolean[ROWS+1][COLS+1][2];
 
         while (!minheap.isEmpty()) {
             Pair<Integer, Pwd> top = minheap.remove();
@@ -220,8 +104,64 @@ public class Day17 {
         return length;
     }
 
-    private static boolean valid(int i, int j, List<String> lines) {
-        return i >= 0 && j >= 0 && i < lines.size() && j < lines.get(i).length();
+    private static void populateGraph(List<String> lines, int mindis, int maxdis) {
+        ROWS = lines.size();
+        COLS = lines.get(0).length();
+        adjacencyListWithWeights = new LinkedHashMap<>();
+        distanceMap = new LinkedHashMap<>();
+
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLS; j++) {
+                // vertical has horizontal neighbors
+                Pwd ver = new Pwd(new Point(i, j), VERTICAL);
+                distanceMap.put(ver, INF);
+                adjacencyListWithWeights.put(ver, new LinkedHashMap<>());
+                int distancep = 0;
+                int distancen = 0;
+                for (int d = 1; d <= maxdis; d++) {
+                    int jj = j + d;
+                    if (valid(i, jj)) {
+                        distancep += valueAt(i, jj, lines);
+                        if (d > mindis) {
+                            adjacencyListWithWeights.get(ver).put(new Pwd(new Point(i, jj), HORIZONTAL), distancep);
+                        }
+                    }
+                    jj = j - d;
+                    if (valid(i, jj)) {
+                        distancen += valueAt(i, jj, lines);
+                        if (d > mindis) {
+                            adjacencyListWithWeights.get(ver).put(new Pwd(new Point(i, jj), HORIZONTAL), distancen);
+                        }
+                    }
+                }
+
+                // horizontal has vertical neighbors
+                Pwd hor = new Pwd(new Point(i, j), HORIZONTAL);
+                distanceMap.put(hor, INF);
+                adjacencyListWithWeights.put(hor, new LinkedHashMap<>());
+                distancep = 0; distancen = 0;
+                for (int d = 1; d <= maxdis; d++) {
+                    int ii = i + d;
+                    if (valid(ii, j)) {
+                        distancep += valueAt(ii, j, lines);
+                        if (d > mindis) {
+                            adjacencyListWithWeights.get(hor).put(new Pwd(new Point(ii, j), VERTICAL), distancep);
+                        }
+                    }
+                    ii = i - d;
+                    if (valid(ii, j)) {
+                        distancen += valueAt(ii, j, lines);
+                        if (d > mindis) {
+                            adjacencyListWithWeights.get(hor).put(new Pwd(new Point(ii, j), VERTICAL), distancen);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private static boolean valid(int i, int j) {
+        return i >= 0 && j >= 0 && i < ROWS && j < COLS;
     }
 
     private static int valueAt(int i, int j, List<String> lines) {
